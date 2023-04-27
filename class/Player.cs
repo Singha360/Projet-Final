c3ce177ced5260af2c3dfa6e7b2a6184a0fa4550
Singha360 <singha360@gmail.com>
2023-04-27 00:06:29 -0400

    Added handling of double entry

--- class/Player.cs
+++ class/Player.cs
@@ -28,58 +28,60 @@ internal class Player : Participant
                 continue;
             }
 
-            if (cellNum < 1 || cellNum > 9)
+            switch (cellNum)
             {
-
-                Console.Clear();
-                board.Print();
-                Console.WriteLine("\nInvalid input. Please enter a number between 1 and 9.");
-
+                case 1:
+                    row = 0;
+                    col = 0;
+                    break;
+                case 2:
+                    row = 0;
+                    col = 1;
+                    break;
+                case 3:
+                    row = 0;
+                    col = 2;
+                    break;
+                case 4:
+                    row = 1;
+                    col = 0;
+                    break;
+                case 5:
+                    row = 1;
+                    col = 1;
+                    break;
+                case 6:
+                    row = 1;
+                    col = 2;
+                    break;
+                case 7:
+                    row = 2;
+                    col = 0;
+                    break;
+                case 8:
+                    row = 2;
+                    col = 1;
+                    break;
+                case 9:
+                    row = 2;
+                    col = 2;
+                    break;
+                default:
+                    Console.Clear();
+                    board.Print();
+                    Console.WriteLine("\nInvalid input. Please enter a number between 1 and 9.");
+                    break;
             }
 
-        } while (cellNum < 1 || cellNum > 9);
-
-        switch (cellNum)
-        {
-            case 1:
-                row = 0;
-                col = 0;
-                break;
-            case 2:
-                row = 0;
-                col = 1;
-                break;
-            case 3:
-                row = 0;
-                col = 2;
-                break;
-            case 4:
-                row = 1;
-                col = 0;
-                break;
-            case 5:
-                row = 1;
-                col = 1;
-                break;
-            case 6:
-                row = 1;
-                col = 2;
-                break;
-            case 7:
-                row = 2;
-                col = 0;
-                break;
-            case 8:
-                row = 2;
-                col = 1;
-                break;
-            case 9:
-                row = 2;
-                col = 2;
-                break;
-        }
+            if (!Char.IsDigit(board.boardMap[row, col]))
+            {
+                Console.Clear();
+                board.Print();
+                Console.WriteLine("\nThis cell is already occupied");
+            }
 
 
+        } while (cellNum < 1 || cellNum > 9 || !Char.IsDigit(board.boardMap[row, col]));
 
         board.boardMap[row, col] = symbol;
     }
